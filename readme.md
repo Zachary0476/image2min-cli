ea67c00c697a3b201380d2308b470fe7a65b6d2d
zlc <15356640476@163.com>
2024-07-05 10:12:32 +0800

    feat:init

---
+++ readme.md
@@ -0,0 +1,38 @@
+# image2min
+
+> Minify images(Base on imagemin-cli)
+
+## Install
+
+```sh
+npm install --global image2min
+```
+
+## Usage
+
+```
+$ image2min --help
+
+  Usage
+    $ image2min <path|glob> ... --out-dir=build [--plugin=<name> ...]
+    $ image2min <file> > <output>
+    $ cat <file> | image2min > <output>
+
+  Options
+    --plugin, -p   Override the default plugins
+    --out-dir, -o  Output directory
+
+  Examples
+    $ image2min images/* --out-dir=build
+    $ image2min foo.png > foo-optimized.png
+    $ cat foo.png | image2min > foo-optimized.png
+    $ image2min foo.png --plugin=pngquant > foo-optimized.png
+    $ image2min foo.png --plugin.pngquant.quality=0.1 --plugin.pngquant.quality=0.2 > foo-optimized.png
+    # Non-Windows platforms may support the short CLI syntax for array arguments
+    $ image2min foo.png --plugin.pngquant.quality={0.1,0.2} > foo-optimized.png
+    $ image2min foo.png --plugin.webp.quality=95 --plugin.webp.preset=icon > foo-icon.webp
+```
+
+## Related
+
+- [image2min](https://github.com/Zachary0476/image2min-cli.git) - API for this module
---
+++ test.js
@@ -0,0 +1,113 @@
+import {promisify} from 'node:util';
+import fs from 'node:fs';
+import {dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';
+import process from 'node:process';
+import {execa} from 'execa';
+import test from 'ava';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+process.chdir(__dirname);
+
+const readFile = promisify(fs.readFile);
+
+test('show version', async t => {
+	const {stdout} = await execa('./cli.js', ['--version']);
+	t.is(stdout, JSON.parse(await readFile('./package.json')).version);
+});
+
+test('optimize a GIF', async t => {
+	const input = await readFile('fixtures/test.gif');
+
+	const {stdout} = await execa('./cli.js', {
+		input,
+		encoding: 'buffer',
+	});
+
+	t.true(stdout.length < input.length);
+});
+
+test('optimize a JPG', async t => {
+	const input = await readFile('fixtures/test.jpg');
+
+	const {stdout} = await execa('./cli.js', {
+		input,
+		encoding: 'buffer',
+	});
+
+	t.true(stdout.length < input.length);
+});
+
+test('optimize a PNG', async t => {
+	const input = await readFile('fixtures/test.png');
+
+	const {stdout} = await execa('./cli.js', {
+		input,
+		encoding: 'buffer',
+	});
+
+	t.true(stdout.length < input.length);
+});
+
+test('optimize a SVG', async t => {
+	const input = await readFile('fixtures/test.svg');
+
+	const {stdout} = await execa('./cli.js', {
+		input,
+		encoding: 'buffer',
+	});
+
+	t.true(stdout.length < input.length);
+});
+
+test('output error on corrupt images', async t => {
+	await t.throwsAsync(execa('./cli.js', ['fixtures/test-corrupt.jpg']));
+});
+
+test('support plugins', async t => {
+	const input = await readFile('fixtures/test.png');
+
+	const {stdout: data} = await execa('./cli.js', ['--plugin=pngquant'], {
+		input,
+		encoding: 'buffer',
+	});
+
+	const {stdout: compareData} = await execa('./cli.js', {
+		input,
+		encoding: 'buffer',
+	});
+
+	t.true(data.length < compareData.length);
+});
+
+test('error when trying to write multiple files to stdout', async t => {
+	const error = await t.throwsAsync(execa('./cli.js', ['fixtures/test.{jpg,png}']));
+	t.is(error.stderr.trim(), 'Cannot write multiple files to stdout, specify `--out-dir`');
+});
+
+test('throw on missing plugins', async t => {
+	const input = await readFile('fixtures/test.png');
+	const error = await t.throwsAsync(execa('./cli.js', ['--plugin=unicorn'], {
+		input,
+		encoding: 'buffer',
+	}));
+
+	t.regex(error.stderr.toString(), /Unknown plugin: unicorn/);
+});
+
+test('support plugin options', async t => {
+	const input = await readFile('fixtures/test.png');
+
+	const {stdout: data} = await execa('./cli.js', ['--plugin.pngquant.dithering=1', '--plugin.pngquant.quality=0.1', '--plugin.pngquant.quality=0.4'], {
+		input,
+		encoding: 'buffer',
+	});
+
+	const {stdout: compareData} = await execa('./cli.js', {
+		input,
+		encoding: 'buffer',
+	});
+
+	t.true(data.length < compareData.length);
+});
